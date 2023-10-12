b60cf0770c8295c55ae26478b25968ce4acc2f7f
fringemonkey89 <markomajeticjr@gmail.com>
2023-10-12 09:07:27 -0400

    added code to dashboard route

---
+++ controllers/api/dashboard-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const sequelize;
+const {Post, User, Comment} = require('../../models')
+//authorization
+const;
+
+router.get('/', ,(req, res) => {
+
+}).then( => {
+
+}).catch(err => {
+
+})
+
+router.get('/edit/:id', ,(req, res) => {
+
+}).then( => {
+
+}).catch(err => {
+    
+})
+
+module.exports = router;
