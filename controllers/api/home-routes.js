7e87641d20171d0411054132b832de3d10d6c13e
fringemonkey89 <markomajeticjr@gmail.com>
2023-10-12 09:01:32 -0400

    added code to home routes

---
+++ controllers/api/home-routes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const sequelize;
+const {Post, User, Comment} = require('../../models')
+
+router.get('/', (req, res) => {
+
+}).then( =>{
+
+}).catch(err = >{
+
+})
+
+router.get('/login', (req, res) => {
+    if() {
+        
+    }
+})
+
+
+router.get('/post/:id', (req, res) => {
+
+}).then( =>{
+
+}).catch(err = >{
+    
+})
---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const dashRoutes;
+const apiRoutes;
+const homeRoutes;
+
+router.use('/api', )
+router.use('/', )
+router.use('/dashboard', )
+
+module.exports = router;
