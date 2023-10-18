bcbfdf6d005c2d5eafabcbf161d2908ed649ce25
fringemonkey89 <113527704+fringemonkey89@users.noreply.github.com>
2023-10-18 14:20:49 -0400

    added code to delete-post.js

---
+++ public/javascript/delete-post.js
@@ -0,0 +1,18 @@
+async function deleteForm (event) {
+
+  event.preventDefault();
+
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length -1];
+  
+  const repsonse = await fetch(`/api/posts/${id}`, {
+    method: 'DELETE'
+  });
+    if(repsonse.ok) {
+      document.location.replace('/profile')
+    } else {
+      alert(response.statusText);
+    }
+}
+
+document.querySelector('').addEventListener('click', deleteForm);
