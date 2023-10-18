00df5dd0b0385dbaf2c9a8f359170f52f494813f
fringemonkey89 <113527704+fringemonkey89@users.noreply.github.com>
2023-10-18 14:05:39 -0400

    added javascript code to add-post.js

---
+++ public/javascript/add-post.js
@@ -0,0 +1,26 @@
+async function newForm (event) {
+  const title = document.querySelector('').value;
+  const content = document.querySelector('').value;
+
+if (title && content){
+  const response = await fetch('/api/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      content
+    }), 
+    headers: {
+      'Content-Type': 'application'
+    }
+  });
+   if(response.ok) {
+     document.location.replace('/profile')
+   } else {
+     alert(response.statusText);
+   }
+}else {
+  alert(please fill out the form)
+}
+}
+
+document.querySelector('').addEventListener('submit', newForm);
