4f51f75d583cb2384b02a96f16f34fc14dce546c
fringemonkey89 <markomajeticjr@gmail.com>
2023-11-01 11:04:03 -0400

    added another route i forgot to implement

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -106,6 +106,28 @@ router.put('/:id', Auth, (req, res) => {
   })
 })
 
+// add a user
+
+router.post('/login', (req, res) => {
+  User.create({
+    username: req.body.username,
+    password: req.body.password
+  })
+  .then(dbUserData => {
+  req.session.save(() => {
+    req.session.user_id = dbUserData.id;
+    req.session.username = dbUserData.username;
+    req.session.loggedIn = true;
+
+    res.json(dbUserData)
+  })
+})
+.catch(err => {
+  console.log(err);
+  res.status(400).json(err)
+})
+})
+
 
 module.exports = router;
 
